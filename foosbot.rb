b1083818401051112ca01bb91d48d648194721dc
Ben Kempner <ben@verticalbrands.com>
2011-12-02 00:42:06 -0800

    foosbotery

--- foosbot.rb
+++ foosbot.rb
@@ -1,3 +1,4 @@
+# encoding: utf-8
 require 'cinch'
 require 'cleverbot'
 
@@ -8,6 +9,7 @@ bot = Cinch::Bot.new do
     c.nick = 'foosbot'
     c.password = 'smellyoulater'
     c.ssl = true
+    #c.channels = ["#foosbot-test"]
     c.channels = ["#verticalbrands foosball", "#foosbot-test"]
 
     @game = false
@@ -17,7 +19,12 @@ bot = Cinch::Bot.new do
   end
 
   on :message, "!foos" do |m|
+
     if not @game
+      #if Random.new.rand(0..6) == 0
+      #  m.reply "I'm sorry Dave, I'm afriad I can't do that." if Random.new.rand(0..3) == 0
+      #  return nil
+      #end
       m.reply "#{m.user.nick} wants to start a game, who's in? Type !foos to join."
       players = m.channel.users.keys.map(&:nick) - @blacklist
       m.reply "#{players.join(', ')} ^"
@@ -27,14 +34,19 @@ bot = Cinch::Bot.new do
       m.reply "#{m.user.nick} joined the game."
       @players << m.user.nick
       if @players.count < 4
-        m.reply "#{4 - @players.count} players still needed."
+        m.reply "#{4 - @players.count} players still needed.  Current players: #{@players.join(', ')}"
       else
         m.reply "game ready: #{@players.join(', ')}"
         @players = []
         @game = false
-        case Random.new.rand(0..4)
+        case Random.new.rand(0..6)
         when 0 then m.reply "kthxbye"
         when 1 then m.reply "Hiyooooo!"
+        when 2 then m.reply "I'm a personality prototype, you can tell can't you?"
+        when 3 then m.reply "Life? Don't talk to me about life."
+        when 4 then m.reply "I'd make a suggestion, but you wouldn't listen.  No one ever does."
+        when 5 then m.reply "I've calculated your chance of survival, but i don't think you'll like it."
+        when 6 then m.reply "This will all end in tears."
         end
       end
     end
@@ -50,6 +62,18 @@ bot = Cinch::Bot.new do
     end
   end
 
+  on :message, "!status" do |m|
+    if @game
+      m.reply "#{4 - @players.count} players still needed.  Current players: #{@players.join(', ')}"
+    else
+      m.reply "no game started."
+    end
+  end
+
+  on :message, "!help" do |m|
+    m.reply "!foos - start or join a game\n!status - check status of current game\n!cancel - cancel current game\n!flip - desk flip\n!help - helpception"
+  end
+
   on :message, /\ rod/ do |m|
     m.reply "That's what she said lol" if Random.new.rand(0..3) == 0
   end
@@ -59,7 +83,11 @@ bot = Cinch::Bot.new do
   end
 
   on :message, /banana/ do |m|
-    m.reply "stfu"
+    case Random.new.rand(0..6)
+    when 0 then m.reply "Hiyooooo!"
+    when 1 then m.reply "stfu"
+    else m.reply "Hilarious!" 
+    end
   end
 
   on :message, /confused/ do |m|
@@ -71,7 +99,7 @@ bot = Cinch::Bot.new do
   end
 
   on :message, /jitterbit/ do |m|
-    m.reply "*snap* *snap*"
+    m.reply "*snap* *snap*" if Random.new(0..1) == 0
   end
 
   on :message, /\ dump\ / do |m|
@@ -88,6 +116,29 @@ bot = Cinch::Bot.new do
 
   on :message, /foosbot/ do |m|
     params = m.reply @cleverbot.write m.message
+  end
+
+  on :message, /nyan/ do |m|
+    m.reply "-_-_-_-_-_-_-_,------,  
+_-_-_-_-_-_-_-|   /\\_/\\  
+-_-_-_-_-_-_-~|__( ^ .^) 
+_-_-_-_-_-_-_-\"\"  \"\"      "
+  end
+
+  on :message, "!flip" do |m|
+    m.reply "（╯°□°）╯︵ ┻━┻"
+  end
+
+  on :message, /desk\ flip|deskflip|flip\ a\ desk/ do |m|
+    m.reply "（╯°□°）╯︵ ┻━┻"
+  end
+
+  on :message, /((h|H)i|(h|H)ey|(s|S)up|(y|Y)o|(h|H)ello) foosbot/ do |m|
+    m.reply "Hello? Yes this is foosbot"
+  end
+
+  on :message, /why/ do |m|
+    m.reply "42" if Random.new.rand(0..2) == 0
   end
 end
 
